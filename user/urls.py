de3775bd5a1a698edd7f9dfe4462d50c4bd70617
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-10-22 03:20:18 +0300

    Define a custom user with email field instead of username

--- user/urls.py
+++ user/urls.py
@@ -10,8 +10,12 @@ from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("profile/", ManageUserView.as_view(), name="profile"),
 ]
