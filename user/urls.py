55591e99a7c311e0b0905f030964a7e63833b44c
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-10-22 03:07:17 +0300

    Add profile endpoint for user to manage their data

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 
 urlpatterns = [
@@ -13,6 +13,7 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("profile/", ManageUserView.as_view(), name="profile"),
 ]
 
 app_name = "user"
