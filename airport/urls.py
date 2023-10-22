ce87953fae4970f75a86a8836aa44301ee540cd8
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-10-23 02:22:05 +0300

    Add basic endpoints, serializers and views for an airport app

--- airport/urls.py
+++ airport/urls.py
@@ -1,1 +1,30 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from airport.views import (
+    AirplaneViewSet,
+    AirplaneTypeViewSet,
+    AirportViewSet,
+    CrewViewSet,
+    FlightViewSet,
+    OrderViewSet,
+    RouteViewSet,
+    TicketViewSet,
+)
+
+router = routers.DefaultRouter()
+
+router.register("airplanes", AirplaneViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airports", AirportViewSet)
+router.register("crews", CrewViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
+router.register("routers", RouteViewSet)
+router.register("tickets", TicketViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
+
 app_name = "airport"
--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,111 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import (
+    Airplane,
+    AirplaneType,
+    Airport,
+    Crew,
+    Flight,
+    Order,
+    Route,
+    Ticket,
+)
+
+from airport.serializers import (
+    AirplaneSerializer,
+    AirplaneListSerializer,
+    AirplaneTypeSerializer,
+    AirportSerializer,
+    CrewSerializer,
+    CrewDetailSerializer,
+    FlightSerializer,
+    FlightListSerializer,
+    FlightDetailSerializer,
+    OrderSerializer,
+    OrderListSerializer,
+    OrderDetailSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    RouteDetailSerializer,
+    TicketSerializer,
+    TicketListSerializer,
+    TicketDetailSerializer,
+)
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AirplaneListSerializer
+        return self.serializer_class
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return CrewDetailSerializer
+        return self.serializer_class
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return FlightListSerializer
+        if self.action == "retrieve":
+            return FlightDetailSerializer
+        return self.serializer_class
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+        if self.action == "retrieve":
+            return OrderDetailSerializer
+        return self.serializer_class
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+        return self.serializer_class
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TicketListSerializer
+        if self.action == "retrieve":
+            return TicketDetailSerializer
+        return self.serializer_class
